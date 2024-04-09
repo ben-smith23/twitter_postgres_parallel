f064b58044216c9c30090ea96e7b45d2120c7336
Ben Smith <bsmith23@cmc.edu>
2024-04-09 13:01:57 -0700

    test10

--- services/pg_normalized_batch/schema.sql
+++ services/pg_normalized_batch/schema.sql
@@ -4,7 +4,6 @@ CREATE EXTENSION postgis;
 
 BEGIN;
 
-
 /*
  * Users may be partially hydrated with only a name/screen_name 
  * if they are first encountered during a quote/reply/mention 
@@ -25,14 +24,14 @@ CREATE TABLE users (
     name TEXT,
     location TEXT,
     description TEXT,
-    withheld_in_countries VARCHAR(2)[],
+    withheld_in_countries VARCHAR(2)[]
 );
 
 /*
  * Tweets may be entered in hydrated or unhydrated form.
  */
 CREATE TABLE tweets (
-    id_tweets BIGINT PRIMARY KEY,
+    id_tweets BIGINT,
     id_users BIGINT,
     created_at TIMESTAMPTZ,
     in_reply_to_status_id BIGINT,
@@ -49,7 +48,7 @@ CREATE TABLE tweets (
     state_code VARCHAR(2),
     lang TEXT,
     place_name TEXT,
-    geo geometry,
+    geo geometry
 
     -- NOTE:
     -- We do not have the following foreign keys because they would require us
@@ -68,13 +67,13 @@ CREATE TABLE tweet_urls (
 
 CREATE TABLE tweet_mentions (
     id_tweets BIGINT,
-    id_users BIGINT,
+    id_users BIGINT
 );
 CREATE INDEX tweet_mentions_index ON tweet_mentions(id_users);
 
 CREATE TABLE tweet_tags (
     id_tweets BIGINT,
-    tag TEXT,
+    tag TEXT
 );
 COMMENT ON TABLE tweet_tags IS 'This table links both hashtags and cashtags';
 CREATE INDEX tweet_tags_index ON tweet_tags(id_tweets);
@@ -83,7 +82,7 @@ CREATE INDEX tweet_tags_index ON tweet_tags(id_tweets);
 CREATE TABLE tweet_media (
     id_tweets BIGINT,
     url TEXT,
-    type TEXT,
+    type TEXT
 );
 
 /*
