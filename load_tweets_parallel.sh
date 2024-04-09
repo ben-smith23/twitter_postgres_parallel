90ca085e22509b32901cd9410ecf2504eae7d047
Ben Smith <bsmith23@cmc.edu>
2024-04-08 23:15:00 -0700

    test6

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -5,14 +5,14 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-time echo "$files" | parallel ./load_denormalized.sh
+echo "$files" | time parallel sh load_denormalized.sh
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time echo "$files" | parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:13462 --inputs {}
+echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:13462 --inputs {}
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-# time echo "$files" | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:13463 --inputs {}
+echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:13463 --inputs {}
